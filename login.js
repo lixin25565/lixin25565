aeac63153fc471965e092bf144e024399a09712a
lixin25565 <102230050+lixin25565@users.noreply.github.com>
2022-12-19 13:42:51 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login() { //登陆判断
-	window.open('https://sczlmc.tk/ZP','_self');
+	window.open('https://z.npop.tk/ZP','_self');
 	window.alert('哈哈哈！~你被骗了！\n Never Gonna Give You Up！~')
 }
 function doClear() { //获取页面所有的input框，是text和password框，内容=空串
